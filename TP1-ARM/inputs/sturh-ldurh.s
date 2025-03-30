d18199a9caf5cbad5278c1a2ad89002c606f4395
fedegutman <federicogutman@Federicos-MacBook-Pro.local>
2025-03-30 19:39:57 -0300

    test

--- TP1-ARM/inputs/sturh-ldurh.s
+++ TP1-ARM/inputs/sturh-ldurh.s
@@ -1,6 +1,10 @@
 .text
-mov X1, 0x1000    
-mov H10, 0x1234      
-sturh H10, [X1, 0x0]  
-ldurh H11, [X1, 0x0] 
-HLT 0           
+mov X1, 0x1000
+lsl X1, X1, 16
+mov X10, 0x1234
+sturh w10, [X1, 0x0]
+sturh W10, [X1, 0x6]
+ldur X13, [X1, 0x0]
+ldurh w14, [X2, 0x4]
+ldurh W15, [X3, 0x6]
+HLT 0
