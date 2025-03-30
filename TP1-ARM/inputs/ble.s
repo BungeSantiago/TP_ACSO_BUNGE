9b8912daa77a55c04d78736350c361d5c3785eee
fedegutman <federicogutman@Federicos-MacBook-Pro.local>
2025-03-30 19:07:38 -0300

    test

--- TP1-ARM/inputs/ble.s
+++ TP1-ARM/inputs/ble.s
@@ -1,12 +1,12 @@
 .text
-movz X0, 2      // X0 = 10
-movz X1, 3      // X1 = 5
-cmp X0, X1       // Compare X0 and X1, set flags accordingly
-bge greater      // If X0 >= X1, branch to 'greater'
+movz X0, 10 
+movz X1, 5  
+cmp X0, X1   
+ble lesser  
 
-// Code here will be skipped if branch is taken
-movz X2, 100     // This instruction is skipped if the branch occurs.
+movz X2, 100    
+HLT 0
 
-greater:
-movz X3, 200     // This instruction is executed if X0 >= X1 (branch is taken)
-hlt 0
+lesser:
+subs X1, X1, 5
+HLT 0
