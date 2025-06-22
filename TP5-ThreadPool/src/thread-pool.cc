e332e1f655d77fbaba38a553680d80bde3893bd3
BungeSantiago <sbunge@udesa.edu.ar>
2025-06-22 15:43:53 -0300

    entrega

--- TP5-ThreadPool/src/thread-pool.cc
+++ TP5-ThreadPool/src/thread-pool.cc
@@ -1,17 +1,103 @@
-/**
- * File: thread-pool.cc
- * --------------------
- * Presents the implementation of the ThreadPool class.
- */
-
 #include "thread-pool.h"
 using namespace std;
 
-ThreadPool::ThreadPool(size_t numThreads) : wts(numThreads), done(false) {}
+ThreadPool::ThreadPool(size_t numThreads) : workers(numThreads) {
+    // armo todos los workers y los dejo libres
+    for (size_t i = 0; i < numThreads; ++i) {
+        idleWorkers.push(i); // lo anoto como disponible
+        readySem.signal(); // sumo uno a la cuenta de libres
+        workers[i].thr = thread(&ThreadPool::worker, this, i); // corro el hilo
+    }
+
+    // levanto el hilo que reparte las tareas
+    dispThr = thread(&ThreadPool::dispatcher, this);
+}
 
 
-void ThreadPool::schedule(const function<void(void)>& thunk) {}
+void ThreadPool::schedule(const function<void(void)>& thunk) {
+    {
+        lock_guard<mutex> lock(tasksMtx);
+        tasks.push(thunk);
+        ++pending;
+    }
+    tasksSem.signal(); // avisamos al dispatcher
+}
 
-void ThreadPool::wait() {}
+void ThreadPool::wait() {
+    unique_lock<mutex> lk(doneMtx);
+    doneCV.wait(lk, [this] { return pending.load() == 0; });
+}
 
-ThreadPool::~ThreadPool() {}
+ThreadPool::~ThreadPool() {
+    wait(); // espera a que se terminen todas las tareas
+    shutting = true; // avisa que vamos a cerrar todo
+
+    // despertamos al dispatcher y a los workers por si están esperando algo
+    tasksSem.signal(); // por si el dispatcher esta dormido
+    for (auto& w : workers) w.sem.signal(); // despertamos a todos los hilos
+
+    if (dispThr.joinable()) dispThr.join(); // esperamos a que el dispatcher termine
+
+    // esperamos a que terminen todos los workers
+    for (auto& w : workers) {
+        if (w.thr.joinable()) w.thr.join();
+    }
+}
+
+
+void ThreadPool::dispatcher() {
+    while (true) {
+        
+        tasksSem.wait(); // espera hasta que llegue una tarea
+        if (shutting && tasks.empty()) break; // si ya cerramos y no hay tareas se va
+
+        // agarro una tarea de la cola
+        function<void(void)> job;
+        {
+            lock_guard<mutex> lock(tasksMtx);
+            if (tasks.empty()) continue; // sigue esperando
+            job = tasks.front();
+            tasks.pop();
+        }
+
+        
+        readySem.wait(); // espero a tener un worker libre
+        size_t wid;
+        {
+            lock_guard<mutex> lock(idleMtx);
+            wid = idleWorkers.front();
+            idleWorkers.pop(); // lo saco de la lista de libres
+        }
+
+        //le paso la tarea al worker y lo despierto
+        workers[wid].task = job;
+        workers[wid].sem.signal();
+    }
+}
+
+
+void ThreadPool::worker(size_t id) {
+    worker_t& self = workers[id];
+
+    while (true) {
+        // esperar tarea o señal de cierre
+        self.sem.wait();
+        if (shutting) break;
+
+        // ejecutar la tarea asignada
+        self.task();
+
+        // actualizar contador de tareas pendientes
+        if (pending.fetch_sub(1) == 1) {
+            lock_guard<mutex> lk(doneMtx);
+            doneCV.notify_all();
+        }
+
+        // marcarse como libre
+        {
+            lock_guard<mutex> lock(idleMtx);
+            idleWorkers.push(id);
+        }
+        readySem.signal();
+    }
+}
--- TP5-ThreadPool/src/thread-pool.h
+++ TP5-ThreadPool/src/thread-pool.h
@@ -1,21 +1,16 @@
-/**
- * File: thread-pool.h
- * -------------------
- * This class defines the ThreadPool class, which accepts a collection
- * of thunks (which are zero-argument functions that don't return a value)
- * and schedules them in a FIFO manner to be executed by a constant number
- * of child threads that exist solely to invoke previously scheduled thunks.
- */
-
 #ifndef _thread_pool_
 #define _thread_pool_
 
-#include <cstddef>     // for size_t
-#include <functional>  // for the function template used in the schedule signature
-#include <thread>      // for thread
-#include <vector>      // for vector
-#include "Semaphore.h" // for Semaphore
+#include <cstddef> 
+#include <functional> 
+#include <queue> 
+#include <thread>
+#include <vector> 
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
 
+#include "Semaphore.h"
 using namespace std;
 
 
@@ -28,65 +23,44 @@ using namespace std;
  * (or condition variable) to signal when work is ready for the 
  * worker to process.
  */
+
 typedef struct worker {
-    thread ts;
-    function<void(void)> thunk;
-    /**
-     * Complete the definition of the worker_t struct here...
-     **/
-} worker_t;
+    thread               thr; // hilo real
+    Semaphore            sem{0}; // señala cuando hay trabajo asignado
+    function<void(void)> task; // tarea a ejecutar
+}worker_t;
 
 class ThreadPool {
-  public:
-
-  /**
-  * Constructs a ThreadPool configured to spawn up to the specified
-  * number of threads.
-  */
-    ThreadPool(size_t numThreads);
-
-  /**
-  * Schedules the provided thunk (which is something that can
-  * be invoked as a zero-argument function without a return value)
-  * to be executed by one of the ThreadPool's threads as soon as
-  * all previously scheduled thunks have been handled.
-  */
+public:
+    explicit ThreadPool(size_t numThreads);
     void schedule(const function<void(void)>& thunk);
-
-  /**
-  * Blocks and waits until all previously scheduled thunks
-  * have been executed in full.
-  */
     void wait();
-
-  /**
-  * Waits for all previously scheduled thunks to execute, and then
-  * properly brings down the ThreadPool and any resources tapped
-  * over the course of its lifetime.
-  */
     ~ThreadPool();
-    
-  private:
 
-    void worker(int id);
+    ThreadPool(const ThreadPool&)            = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+
+private:
+    // funciones auxiliares que corren en hilos aparte
     void dispatcher();
-    thread dt;                              // dispatcher thread handle
-    vector<worker_t> wts;                   // worker thread handles. you may want to change/remove this
-    bool done;                              // flag to indicate the pool is being destroyed
-    mutex queueLock;                        // mutex to protect the queue of tasks
+    void worker(size_t id);
 
-    /* It is incomplete, there should be more private variables to manage the structures... 
-    * *
-    */
-  
-    /* ThreadPools are the type of thing that shouldn't be cloneable, since it's
-    * not clear what it means to clone a ThreadPool (should copies of all outstanding
-    * functions to be executed be copied?).
-    *
-    * In order to prevent cloning, we remove the copy constructor and the
-    * assignment operator.  By doing so, the compiler will ensure we never clone
-    * a ThreadPool. */
-    ThreadPool(const ThreadPool& original) = delete;
-    ThreadPool& operator=(const ThreadPool& rhs) = delete;
+
+    queue<function<void(void)>> tasks; // cola FIFO de thunks
+    mutex                tasksMtx; // protege tasks
+    Semaphore            tasksSem{0}; // cuenta tareas en cola
+
+    queue<size_t>        idleWorkers; // IDs de workers libres
+    mutex                idleMtx; // protege idleWorkers
+    Semaphore            readySem{0}; // cuenta workers libres
+
+    vector<worker_t>     workers; // todos los workers
+    thread               dispThr; // hilo despachador
+
+    atomic<size_t>       pending{0}; // tareas que faltan
+    mutex                doneMtx; // para wait
+    condition_variable   doneCV;
+
+    atomic<bool>         shutting{false}; // cierre del pool
 };
 #endif
